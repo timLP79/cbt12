42c0f7412585819cc8f55f85ee2f356dc4321e42
Tim Palacios <timpalacios@u.boisestate.edu>
2025-12-30 21:49:10 -0700

    Fix database URL environment variable for render

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     """Base configuration"""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or os.environ.get(
         'DATABASE_URI') or f'sqlite:///{os.path.join(basedir, "instance", "cbt_assessment.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
